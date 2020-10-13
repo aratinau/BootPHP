dca67abc4c327c3a11a55af48667bd69e80fab06
Aymeric <hello@codeparadise.xyz>
2020-10-13 14:55:01 +0200

    create file by cms page

--- demo/cms.php
+++ demo/cms.php
@@ -1,5 +1,14 @@
-<form action="" method="post">
+<?php
+if (isset($_POST["path_name"]) && isset($_POST["content"]) && !empty($_POST["path_name"]) && !empty($_POST["path_name"])) {
+    $path = DEMO_PATH . DIRECTORY_SEPARATOR . $_POST["path_name"] . '.php';
+    $handle = fopen($path, 'w');
+    fwrite($handle, $_POST["content"]);
+    fclose($handle);
+    header('Location: '.$current_path_name);
+}
+?>
+<form action="<?php echo $current_path_name; ?>" method="post">
     Nom fichier <input type="text" name="path_name" /> .php
-    <textarea name="conten" style="width: 100%" rows="30"></textarea>
+    <textarea name="content" style="width: 100%" rows="30"></textarea>
     <input type="submit" />
 </form>
