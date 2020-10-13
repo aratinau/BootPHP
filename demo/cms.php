81f180f647db9baaf3fd9b0e4dacd0c74dc5f9a1
Aymeric <hello@codeparadise.xyz>
2020-10-13 12:45:06 +0200

    cmd file

---
+++ demo/cms.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    Nom fichier <input type="text" name="path_name" /> .php
+    <textarea name="conten" style="width: 100%" rows="30"></textarea>
+    <input type="submit" />
+</form>
