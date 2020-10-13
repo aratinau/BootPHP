58a0f6d0d75757427144705d9d12e6eae06d154c
Aymeric <hello@codeparadise.xyz>
2020-10-13 09:19:08 +0200

    include navbar

--- index.php
+++ index.php
@@ -16,7 +16,10 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 	</head>
 	<body class="">
+    <?php require('navbar.php'); ?>
+    <div class="container">
 		<h1>Hello</h1>
+    </div>
 	</body>
 </html>
 
